f27376a94cf313e677887efa00956bd1979d3758
FrancoRosa <km115.franco@gmail.com>
2020-03-30 11:09:41 -0500

    Delete bubble_sort method

--- main.rb
+++ main.rb
@@ -1,27 +1,15 @@
 def bubble_sort(array)
   unordered_array_size = array.size;
-  
   array.size.times do
     ordered = true
     index = 0
-
     while index < unordered_array_size - 1
-      value1 = array[index]
-      value2 = array[index + 1]
-      if array[index].is_a?String
-        value1 = array[index].length
-      end
-      if array[index + 1].is_a?String
-        value2 = array[index + 1].length
-      end
-
-      if value1 > value2
+      if array[index] > array[index + 1]
         array[index], array[index + 1] = array[index + 1], array[index]
         ordered = false
       end
       index += 1
     end
-    
     unordered_array_size -= 1 # the last item will always be ordered
     ordered ? break : next  # if there were no swaps, the array is ordered - finish loop
   end
@@ -30,30 +18,27 @@ end
 
 def bubble_sort_by(array)
   unordered_array_size = array.size;
-  
   array.size.times do
     ordered = true
     index = 0
-
     while index < unordered_array_size - 1
-
       if yield(array[index], array[index+1]) > 0
         array[index], array[index + 1] = array[index + 1], array[index]
         ordered = false
       end
       index += 1
     end
-    
     unordered_array_size -= 1 # the last item will always be ordered
     ordered ? break : next  # if there were no swaps, the array is ordered - finish loop
   end
-  print array
+  return array
 end
 
 
-#print bubble_sort([2, 1, -2, "vluea", "a", 64, 23, 11, 4, 44])
-
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
-  right.length - left.length 
+result = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length 
 end
 
+puts bubble_sort([2, 1, -2, 64, 23, 11, 4, 44]).inspect
+
+puts result.inspect
