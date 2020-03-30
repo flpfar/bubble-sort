07050d765f41ef75014768e659450aebd6408f2e
FrancoRosa <km115.franco@gmail.com>
2020-03-30 11:39:43 -0500

    Fix linter with rubocop

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 def bubble_sort(array)
-  unordered_array_size = array.size;
+  unordered_array_size = array.size
   array.size.times do
     ordered = true
     index = 0
@@ -11,32 +11,31 @@ def bubble_sort(array)
       index += 1
     end
     unordered_array_size -= 1 # the last item will always be ordered
-    ordered ? break : next  # if there were no swaps, the array is ordered - finish loop
+    ordered ? break : next # if there were no swaps, the array is ordered - finish loop
   end
-  return array
+  array
 end
 
 def bubble_sort_by(array)
-  unordered_array_size = array.size;
+  unordered_array_size = array.size
   array.size.times do
     ordered = true
     index = 0
     while index < unordered_array_size - 1
-      if yield(array[index], array[index+1]) > 0
+      if yield(array[index], array[index + 1]) > 0
         array[index], array[index + 1] = array[index + 1], array[index]
         ordered = false
       end
       index += 1
     end
     unordered_array_size -= 1 # the last item will always be ordered
-    ordered ? break : next  # if there were no swaps, the array is ordered - finish loop
+    ordered ? break : next # if there were no swaps, the array is ordered - finish loop
   end
-  return array
+  array
 end
 
-
-result = bubble_sort_by(["hi","hello","hey"]) do |left,right|
-  left.length - right.length 
+result = bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
 end
 
 puts bubble_sort([2, 1, -2, 64, 23, 11, 4, 44]).inspect
