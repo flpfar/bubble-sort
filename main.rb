17a5972abe22a0f8db0c0c3dcd6afbddc1c459d6
Felipe Rosa <flp.far@gmail.com>
2020-03-30 12:49:09 -0300

    Add bubble_sort_by with yield

--- main.rb
+++ main.rb
@@ -25,9 +25,35 @@ def bubble_sort(array)
     unordered_array_size -= 1 # the last item will always be ordered
     ordered ? break : next  # if there were no swaps, the array is ordered - finish loop
   end
-
   return array
 end
 
-print bubble_sort([2, 1, -2, "vluea", "a", 64, 23, 11, 4, 44])
+def bubble_sort_by(array)
+  unordered_array_size = array.size;
+  
+  array.size.times do
+    ordered = true
+    index = 0
+
+    while index < unordered_array_size - 1
+
+      if yield(array[index], array[index+1]) > 0
+        array[index], array[index + 1] = array[index + 1], array[index]
+        ordered = false
+      end
+      index += 1
+    end
+    
+    unordered_array_size -= 1 # the last item will always be ordered
+    ordered ? break : next  # if there were no swaps, the array is ordered - finish loop
+  end
+  print array
+end
+
+
+#print bubble_sort([2, 1, -2, "vluea", "a", 64, 23, 11, 4, 44])
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  right.length - left.length 
+end
 
